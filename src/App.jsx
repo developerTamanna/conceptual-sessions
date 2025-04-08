1e0c43f7845ec57f6a659fbbc223bc49c45a9098
developerTamanna <astamanna57@gmail.com>
2025-04-08 11:10:57 +0600

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import './App.css'
 import LoadButton from './components/LoadButton/LoadButton'
 import Navbar from './components/Navbar/Navbar'
 import ShowCategoriesFood from './components/ShowCategoriesFood/ShowCategoriesFood'
+import Footer from './components/Footer/Footer'
+import Hero from './components/Hero/Hero'
 
 function App() {
    const [selectCategory, setSelectedCategory] = useState("")
@@ -17,10 +19,12 @@ function App() {
    <LoadButton
     setSelectedCategory ={setSelectedCategory}
    ></LoadButton>
-
+     <Hero></Hero>
    <ShowCategoriesFood
       selectCategory={selectCategory}
    ></ShowCategoriesFood>
+
+   <Footer></Footer>
     
     </>
   )
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div>
+            <div className="hero bg-base-200 h-60 mt-20">
+  <div className="hero-content text-center">
+    <div className="max-w-md">
+      <h2 className="text-2xl font-bold">Please Select On Category</h2>
+      
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Hero;
